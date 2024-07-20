a34d00a68bfe7f29e3800cbd360912603a0e1c9d
Szymii <szymimelzer9@gmail.com>
2024-07-20 19:51:34 +0200

    Moving and jumping animations

---
+++ scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+ 
+var direction: float = 0
+ 
+@onready var animation_player = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+func paly_animation():
+	if direction != 0 and is_on_floor():
+		animation_player.play("move")
+		return 
+
+	if !is_on_floor():
+		animation_player.play("jump")
+		return 
+		
+	animation_player.play("RESET")
+	return 
+
+func _physics_process(_delta):
+	direction = Input.get_axis("left", "right")
+	
+	print(direction != 0)
+
+	if direction != 0:
+		sprite.flip_h = (direction == - 1)
+ 
+	paly_animation()
