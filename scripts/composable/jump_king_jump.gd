0a6dc28542d98de25674e07be6960ba4b246766f
Szymii <szymimelzer9@gmail.com>
2024-07-21 17:02:04 +0200

    Bouncing off the walls

--- scripts/composable/jump_king_jump.gd
+++ scripts/composable/jump_king_jump.gd
@@ -2,10 +2,12 @@ class_name JumpKingJump extends Node
 
 @export var speed: float = 200
 @export var base_jump_height: float = 10
+@export var max_jump_height: float = 40
 
 var is_jumping: bool = false;
 var jump_direction = 1 # 1 = right, -1 = left
 var jump_height: float = base_jump_height
+var jump_angle = 2
 
 func _enter_tree() -> void:
 	# This component can only be a chld of CharacterBody2D
@@ -16,10 +18,10 @@ func _exit_tree() -> void:
 	owner.remove_meta(&"InteractableComponent") # Unregister
 
 func time_to_peak() -> float:
-	return (0.05 * jump_height / 10) + 0.10
+	return (0.05 * jump_height / 5)
 
 func time_to_fall() -> float:
-	return (0.05 * jump_height / 10) + 0.05
+	return (0.05 * jump_height / 4)
 
 func jump_velocity() -> float:
 	return ((2.0 * jump_height) / time_to_peak())
@@ -27,18 +29,23 @@ func jump_velocity() -> float:
 func get_gravity() -> float:
 	var _time_to_pick = time_to_peak()
 	var _time_to_fall = time_to_fall()
+	
 	if owner.velocity.y < 0:
 		return ((2.0 * jump_height) / (_time_to_pick * _time_to_pick * 2))
 	else:
-		return ((2.0 * jump_height) / (_time_to_fall * _time_to_fall * 2))
+		return ((2.0 * jump_height) / (_time_to_fall * _time_to_fall))
 
 func jump() -> void:
-	if Input.is_action_pressed("jump") and jump_height < 100:
-		jump_height += 1
+	if Input.is_action_pressed("jump") and jump_height < max_jump_height:
+		# 3 sec is max jump loading time
+		jump_height += (max_jump_height - base_jump_height) / 90
 
 	if Input.is_action_just_released("jump"):
 		is_jumping = true
 		owner.velocity.y = -jump_velocity()
+
+func move() -> void:
+	owner.velocity.x = clamp(Input.get_axis("left", "right") * 100, -1.0, 1.0) * speed
 
 func set_jump_direction() -> void:
 		if Input.is_action_just_pressed("left"):
@@ -46,23 +53,29 @@ func set_jump_direction() -> void:
 		elif Input.is_action_just_pressed("right"):
 			jump_direction = 1
 
+func bounce_off_wall() -> void:
+	if owner.is_on_wall():
+		jump_direction = jump_direction * - 1
+
+func reset_jump() -> void:
+	is_jumping = false
+	jump_height = base_jump_height
+
 func _physics_process(delta) -> void:
 	if owner.is_on_floor():
-		# reset jump
-		if is_jumping:
-			is_jumping = false
-			jump_height = base_jump_height
-
-		# move left or right
-		owner.velocity.x = clamp(Input.get_axis("left", "right") * 100, -1.0, 1.0) * speed
 		
+		if is_jumping:
+			reset_jump()
+
+		move()
 		set_jump_direction()
 		jump()
 			
 	else:
 		owner.velocity.y += get_gravity() * delta
 		if is_jumping:
-			owner.velocity.x = jump_direction * speed / 1.5
+			bounce_off_wall()
+			owner.velocity.x = jump_direction * speed / jump_angle
 		else:
 			# fall off the edge (slight controll)
 			owner.velocity.x = clamp(Input.get_axis("left", "right") * 100, -1.0, 1.0) * speed / 4
--- scripts/player.gd
+++ scripts/player.gd
@@ -8,8 +8,8 @@ var direction: float = 0
 @onready var free_jump = $FreeJump
 @onready var jump_king_jump = $JumpKingJump
 
-
 func paly_animation():
+	direction = Input.get_axis("left", "right")
 	if direction != 0 and is_on_floor():
 		animation_player.play("move")
 		return
@@ -28,9 +28,8 @@ func _ready():
 		remove_child(jump_king_jump)
 
 func _physics_process(_delta):
-	direction = Input.get_axis("left", "right")
-	
-	if direction != 0:
-		sprite.flip_h = (direction == - 1)
+	if velocity.x != 0:
+		print(velocity.x < 0)
+		sprite.flip_h = (velocity.x < 0)
  
 	paly_animation()
